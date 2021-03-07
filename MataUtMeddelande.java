013b75cf9a80cd4de48a47befd900a1896f03be6
ninita10 <80218710+ninita10@users.noreply.github.com>
2021-03-07 21:46:15 +0100

    Add files via upload

---
+++ MataUtMeddelande.java
@@ -0,0 +1,19 @@
+class MataUtMeddelande
+{
+	public static void main(String[] args)
+	{
+		//flera meddelande i olika rader
+		System.out.println("Vi lyckades alltid");
+		System.out.println("Vi lyckas alltid");
+		System.out.println("Vi kommer alltid att lyckas!");
+
+		//en tom rad
+		System.out.println();
+
+		// felra meddelande i samma rad
+		System.out.print("Vi lyckades alltid. ");
+		System.out.print("Vi lyckas alltid. ");
+		System.out.print("Vi kommer alltid att lyckas! ");
+		System.out.println();
+	}
+}
